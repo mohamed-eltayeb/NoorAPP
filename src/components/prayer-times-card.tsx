682f6175a65818803fb15184f21ae90ae8fc2aef
Mohamed Eltayib <mohamed@zagroup.tech>
2025-07-04 17:10:29 +0000

    qibla direction should also be as acurate as possible , the direction of

--- src/components/prayer-times-card.tsx
+++ src/components/prayer-times-card.tsx
@@ -133,7 +133,7 @@ export function PrayerTimesCard() {
                             className="w-5 h-5 flex items-center justify-center transition-transform"
                             style={{ transform: `rotate(${data.qibla}deg)`}}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="hsl(var(--primary))" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="hsl(var(--primary))" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-navigation-2"><polygon points="12 2 19 21 12 17 5 21 12 2"/></svg>
                         </div>
                     </div>
                 </div>
